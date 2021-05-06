e9512c1a25c4400b072a21fd84f7e76d5a5f7765
vickyjaiswal647 <vickyjaiswal647@gmail.com>
2021-05-07 01:43:11 +0530

    Implemented firebase utils, included ability to store and authenticate users into our firestore database

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import Header from './components/header/header.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   constructor() {
@@ -20,11 +20,26 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+            
+          }
+          
+          );
+          console.log(this.state);
+        });
+        
+      }
+      this.setState({ currentUser: userAuth});
+    });
   }
 
   componentWillUnmount() {
@@ -35,7 +50,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+      <Header currentUser={this.state.currentUser}/>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,12 +5,40 @@ import 'firebase/auth';
 const config = {
     apiKey: "AIzaSyCivHlk6xNspNMlbJpq0seRvRZgVIYY2-g",
     authDomain: "style-clothing-ffe89.firebaseapp.com",
+    databaseURL: "https://style-clothing-ffe89-default-rtdb.firebaseio.com",
     projectId: "style-clothing-ffe89",
     storageBucket: "style-clothing-ffe89.appspot.com",
     messagingSenderId: "486510304949",
     appId: "1:486510304949:web:54811537331374f41ca8fc",
     measurementId: "G-X4NY46GY8S"
   };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch ( error ) {
+        console.log('error creating user', error.message);
+      }
+    }
+
+    return userRef;
+
+  }
 
   firebase.initializeApp(config);
 
